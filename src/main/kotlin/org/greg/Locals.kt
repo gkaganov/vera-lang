ff5f46d81ef3efe4f284f6652b84b9e5d89aac9c
gkaganov <gkaganov@tuta.com>
2026-05-03 13:58:02 +0200

    store and load now respect type

--- src/main/kotlin/org/greg/Locals.kt
+++ src/main/kotlin/org/greg/Locals.kt
@@ -10,12 +10,13 @@ class Locals {
     private val locals: MutableMap<Name, Local> = mutableMapOf()
     private var nextFreeSlot = Slot(0)
 
-    fun declare(name: Name, type: VeraAst.Type): Slot {
-        val currentSlot = nextFreeSlot
+    fun declare(name: Name, type: VeraAst.Type): Local {
         require(name !in locals.keys) { "Duplicate local name: '$name'" }
-        locals[name] = Local(nextFreeSlot, type)
+
+        val newLocal = Local(nextFreeSlot, type)
+        locals[name] = newLocal
         nextFreeSlot = Slot(nextFreeSlot.id + 1)
-        return currentSlot
+        return newLocal
     }
 
     fun getLocal(name: String): Local? = locals[Name(name)]
